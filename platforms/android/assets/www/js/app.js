14158f4048344235f4865e2e3ca86fa94b2424d0
Joel Andy <joelunmsm@gmail.com>
2016-06-07 00:41:11 -0500

    joel

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -127,17 +127,19 @@ angular.module('starter', ['ionic','ngCordova','firebase','ngStorage'])
 
       var ref = new Firebase("https://monitoreo.firebaseio.com/gamarra/codigo");
 
-      console.log('SIn',sl.$id)
-
       var refmodelo = ref.child(sl.$id);
 
       refmodelo.update({
     
           estado:'Vendido',
           fventa : currentTime,
-          excel:'1'
+          excel:'1',
+          background:'#5EC960',
+          colortext:'#fff',
+          venta:false
        
       })
+
 
 
     var connectedRef = new Firebase("https://monitoreo.firebaseio.com/.info/connected");
@@ -169,7 +171,7 @@ angular.module('starter', ['ionic','ngCordova','firebase','ngStorage'])
         ref.orderByChild("id").equalTo(imageData.text).on("child_added", function(f) {
         console.log(f.key(),f.val());
 
-        $scope.searchText = f.val().modelo
+        $scope.searchText = f.val().id
 
       });
     
@@ -183,7 +185,7 @@ angular.module('starter', ['ionic','ngCordova','firebase','ngStorage'])
 
 
 
-      $ionicModal.fromTemplateUrl('my-modal.html', {
+  $ionicModal.fromTemplateUrl('my-modal.html', {
     scope: $scope,
     animation: 'slide-in-up'
   }).then(function(modal) {
@@ -191,6 +193,8 @@ angular.module('starter', ['ionic','ngCordova','firebase','ngStorage'])
   });
   $scope.openModal = function() {
     $scope.modal.show();
+
+
   };
   $scope.closeModal = function() {
     $scope.modal.hide();
@@ -206,6 +210,123 @@ angular.module('starter', ['ionic','ngCordova','firebase','ngStorage'])
   // Execute action on remove modal
   $scope.$on('modal.removed', function() {
     // Execute action
+  });
+
+
+
+  $ionicModal.fromTemplateUrl('ubicacion.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(ubicacion) {
+    $scope.ubicacion = ubicacion;
+
+
+  });
+
+
+
+  $scope.openModal = function() {
+    $scope.ubicacion.show();
+    console.log('item',$scope.item)
+
+   
+
+  };
+
+  $scope.torre =function(data){
+
+    data.centro = false
+    data.almacen = false
+    console.log(data)
+    $http.get("http://192.241.255.109:1000/ubica/"+data.id+'/Torre/').success(function(response) {});
+
+
+    var currentTime = new Date();
+    var ref = new Firebase("https://monitoreo.firebaseio.com/gamarra/codigo");
+    var refmodelo = ref.child(data.id);
+    refmodelo.update({
+  
+        ubicacion:'Torre',
+        ftraslado : currentTime,
+      
+    })
+
+
+  }
+
+    $scope.centro =function(data){
+
+    data.torre = false
+    data.almacen = false
+    console.log(data)    
+    $http.get("http://192.241.255.109:1000/ubica/"+data.id+'/Centro/').success(function(response) {});
+
+    var currentTime = new Date();
+    var ref = new Firebase("https://monitoreo.firebaseio.com/gamarra/codigo");
+    var refmodelo = ref.child(data.id);
+    refmodelo.update({
+  
+        ubicacion:'Centro',
+        ftraslado : currentTime,
+      
+    })
+
+  }
+
+    $scope.almacen =function(data){
+
+    data.torre = false
+    data.centro = false
+    console.log(data)    
+    $http.get("http://192.241.255.109:1000/ubica/"+data.id+'/Almacen/').success(function(response) {});
+
+    var currentTime = new Date();
+    var ref = new Firebase("https://monitoreo.firebaseio.com/gamarra/codigo");
+    var refmodelo = ref.child(data.id);
+    refmodelo.update({
+  
+        ubicacion:'Almacen',
+        ftraslado : currentTime,
+      
+    })
+
+  }
+
+
+  $scope.traslado=function(data){
+
+    console.log('data',data)
+    $scope.ubicacion.show()
+    $scope.changetraslado = data
+
+    if (data.ubicacion == 'Torre'){
+
+      $scope.changetraslado.torre = true
+    }
+    if (data.ubicacion == 'Almacen'){
+     
+      $scope.changetraslado.almacen = true 
+    }    
+    if (data.ubicacion == 'Centro'){
+
+      $scope.changetraslado.centro = true
+      
+    }
+
+
+  }
+
+
+  $scope.closeModal = function() {
+    $scope.ubicacion.hide();
+  };
+  // Execute action on hide modal
+  $scope.$on('ubicacion.hidden', function() {
+    console.log('hisee')
+  });
+  // Execute action on remove modal
+  $scope.$on('ubicacion.removed', function() {
+    console.log('remode')
   });
  
 });
